b635fe6403e2f5e0f3bfab91e3a8d229aa16723b
Alan-J-Bibins <alanjbibins@gmail.com>
2025-03-27 12:12:48 +0530

    fix: Width Transition

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,15 +6,16 @@ import { useState } from "react";
 function SidebarItem({ href, Icon, label, isExpanded }: { href: string, label: string, Icon: LucideIcon, isExpanded: boolean }) {
     const pathname = useLocation().pathname.split('/')[1];
     return (
-        <Link to={href} className={`grid w-full gap-2 p-4 justify-items-start items-center content-center transition-all duration-200
+        <Link to={href} className={`grid gap-2 p-4 justify-items-start items-center content-center transition-all duration-200
         rounded-2xl border border-transparent group-hover:bg-secondary/10 group-hover:border-secondary/40
         group-hover:hover:bg-secondary/40
         ${isExpanded ? 'grid-cols-[auto_1fr] w-32' : 'grid-cols-1 w-14'}
-        ${pathname === href.split('/')[1] ? 'border-t-primary/20 border-b-primary dark:border-b-accent border-x-secondary/20 group-hover:border-t-primary/20 group-hover:border-b-primary group-hover:dark:border-b-accent group-hover:border-x-secondary/20 shadow-xl dark:shadow-accent/25 shadow-primary/25' : ''}`}
+        ${pathname === href.split('/')[1]
+                ? 'border-t-primary/20 border-b-primary dark:border-b-accent border-x-secondary/20 group-hover:border-t-primary/20 group-hover:border-b-primary group-hover:dark:border-b-accent group-hover:border-x-secondary/20 shadow-xl dark:shadow-accent/25 shadow-primary/25'
+                : ''}`}
         >
             <Icon size={20} className="justify-self-start mx-px" />
             <p
-                id="linkLabel"
                 className={`${isExpanded
                     ? 'motion-opacity-in-0 -motion-translate-x-in-50 motion-duration-150'
                     : 'motion-translate-x-out-100 motion-duration-200 hidden'}`}
