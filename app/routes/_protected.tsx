b39923814397d81b73bd02620bcf4e4b4514d4af
Alan-J-Bibins <alanjbibins@gmail.com>
2025-03-27 13:17:40 +0530

    fix: Name prop

--- app/routes/_protected.tsx
+++ app/routes/_protected.tsx
@@ -1,11 +1,24 @@
-import { Outlet } from "@remix-run/react";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
+import Searchbar from "components/Searchbar";
 import Sidebar from "components/Sidebar";
+import { authenticator } from "~/services/auth.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+    const user = await authenticator.isAuthenticated(request, {
+        failureRedirect: "/"
+    });
+    return { user };
+};
 
 export default function Layout() {
+    const { user } = useLoaderData<typeof loader>();
+    const name = user.name;
     return (
         <main className="flex gap-4 h-full">
-            <Sidebar />
-            <div className="bg-gradient-to-r from-secondary/20 to-secondary/35 border-l border-primary/20 p-8 rounded-l-2xl w-full h-full">
+            <Sidebar name={name}/>
+            <div className="space-y-2 bg-gradient-to-r from-secondary/20 to-secondary/35 border-l border-primary/20 p-8 rounded-l-2xl w-full h-full">
+                <Searchbar />
                 <Outlet />
             </div>
         </main>
