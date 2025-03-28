6b956878be395b38565e6344eb36b7a7d4090348
Sujatro <88082977+iamsurjog@users.noreply.github.com>
2025-03-28 23:28:22 +0530

    Added KanbanBox style
    styled Kanban Box basic and demo in projects

--- app/routes/_protected.projects.tsx
+++ app/routes/_protected.projects.tsx
@@ -1,7 +1,7 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import { authenticator } from "~/services/auth.server";
-
+import KanbanBox from "components/KanbanBox"
 export const loader = async ({ request }: LoaderFunctionArgs) => {
     const user = await authenticator.isAuthenticated(request, {
         failureRedirect: "/"
@@ -15,7 +15,8 @@ export default function Page() {
 
     return (
         <div>
-            Projects Page
+          Projects Page
+        <KanbanBox  task_name="task name" task_description= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si." logo= "" tags={["hello", "byebye", "category 1", "2", "VERY LONG CATEGORY NAME"]} date = "9/11/2001" />
             <p>Welcome {user.name}</p>
             <Form action="/logout" method="post">
                 <button type="submit">
--- components/KanbanBox.tsx
+++ components/KanbanBox.tsx
@@ -13,24 +13,31 @@ export default function KanbanBox({
 }) {
 
     return (
-        <div className="bg-white/20 border rounded-2xl p-4">
-            <div className="title">
-                <h3>{task_name}</h3>
-                <img src={logo} alt="logo" />
+        <div className="flex items-start justify-start rounded-2xl p-4 border border-gray-300">
+            <div className="flex items-center justify-between mb-2">
+                <h3 className="font-bold text-lg">{task_name}</h3>
+                <img src={logo} alt="logo" className="w-6 h-6 rounded-full" />
             </div>
-            <div className="description">
-                <p>{task_description}</p>
+            <hr></hr>
+            <p className="text-sm mb-4">
+                {task_description}
+            </p>
+            <div className="flex justify-between items-center mb-2">
+           
+            <div className="flex flex-wrap gap-2 mb-4">
+                {tags.map((tag, index) => (
+                    <span 
+                        key={index} 
+                        className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full"
+                    >
+                        {tag}
+                    </span>
+                ))}
             </div>
-            <div className="description">
-                {tags.map((tag, index) => {
-                    return (
-                        <span key={index}>
-                            {tag}
-                        </span>
-                    );
-                })}
+            
+                {date}
             </div>
-            <p>{date}</p>
         </div>
     )
 }
+
