59cabc53d55d27845bbf6bbe7ca1da7a9b170aeb
Alan-J-Bibins <alanjbibins@gmail.com>
2025-03-29 12:43:10 +0530

    feat: Added BreadCrumb and Button Components

---
+++ components/BreadCrumbs.tsx
@@ -0,0 +1,10 @@
+import { useLocation } from "@remix-run/react"
+
+export default function BreadCrumbs() {
+    const pathname = useLocation().pathname.split('/');
+    console.log(pathname);
+    return (
+        <div className="text-primary">{pathname[1][0].toUpperCase()+pathname[1].slice(1)}</div>
+    )
+}
+
