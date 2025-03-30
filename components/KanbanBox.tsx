6b6f10ec5abfca1bd885771c3b8adaa6ba7b3d97
Sujatro <88082977+iamsurjog@users.noreply.github.com>
2025-03-30 20:15:38 +0530

    feat: style for KanbanBox

--- components/KanbanBox.tsx
+++ components/KanbanBox.tsx
@@ -13,12 +13,12 @@ export default function KanbanBox({
 }) {
 
     return (
-        <div className="flex flex-col items-start justify-start rounded-2xl p-4 border border-gray-300">
+        <div className="flex flex-col items-start justify-start rounded-2xl p-4 border border-gray-300 border-primary/20 rounded-2xl bg-secondary/20 bg-gradient-to-b from-transparent to-primary/10 shadow-primary/25 shadow-none hover:bg-gradient-to-t hover:from-accent/20 hover:to-transparent">
             <div className="flex w-full items-center justify-between mb-2">
                 <h3 className="font-bold text-lg">{task_name}</h3>
                 <img src={logo} alt="logo" className="w-6 h-6 rounded-full" />
             </div>
-            <hr className="w-full"></hr>
+            <hr className="w-full border-text/20"></hr>
             <p className="text-sm mb-4">
                 {task_description}
             </p>
@@ -28,14 +28,15 @@ export default function KanbanBox({
                 {tags.map((tag, index) => (
                     <span 
                         key={index} 
-                        className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full"
+                        className="bg-secondary px-3 py-1 rounded-full"
                     >
                         {tag}
                     </span>
                 ))}
-            </div>
-            
+            </div >
+            <div className="text-primary">
                 {date}
+            </div>
             </div>
         </div>
     )
