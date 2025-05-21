2e9a5e5a288babd31d0106148c696953a4689781
Alan-J-Bibins <alanjbibins@gmail.com>
2025-05-22 00:16:03 +0530

    chore: Remove unnecessary imports and logs

--- app/components/BreadCrumbs.tsx
+++ app/components/BreadCrumbs.tsx
@@ -2,7 +2,6 @@ import { useLocation } from 'react-router';
 
 export default function BreadCrumbs() {
     const pathname = useLocation().pathname.split('/');
-    console.log(pathname);
     return (
         <div className="text-primary">
             {pathname[1][0].toUpperCase() + pathname[1].slice(1)}
